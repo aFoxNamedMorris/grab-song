7cb7896c9f78d6b0176171775573f2caf1ba9ec1
aFoxNamedMorris <afoxnamedmorris@gmail.com>
2016-07-24 03:30:41 -0400

    Fixed a useless break.

--- grab-song.sh
+++ grab-song.sh
@@ -65,7 +65,7 @@ if [ -z "$PLAYER_SELECTION" ]; then
   sh check-media-players.sh
   printf "\n"
   exit
-else break; fi
+fi
 
   OUTPUT_DIR=${OUTPUT_DIR-$(cat $SETTINGS_FILE | grep "output-directory=" | sed 's/output-directory=//')}
 
